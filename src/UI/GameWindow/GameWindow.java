6e2e17fd35f4797fc09532a7ed8f36ed00bf68b1
Cupofme <danil.ilyashuk@gmail.com>
2018-07-02 00:53:12 +0300

    Added primitive start window

--- src/UI/GameWindow/GameWindow.java
+++ src/UI/GameWindow/GameWindow.java
@@ -2,11 +2,26 @@ package UI.GameWindow;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class GameWindow extends JFrame {
     public GameWindow() {
+        init();
+        setMenuBars();
+    }
+
+    private void setMenuBars() {
+        JMenuBar menuBar = new JMenuBar();
+        JMenu fileMenu = new JMenu("Файл");
+        JMenuItem openItem = new JMenuItem("Открыть");
+        JMenuItem saveItem = new JMenuItem("Coxранить");
+        fileMenu.add(openItem);
+        fileMenu.add(saveItem);
+        menuBar.add(fileMenu);
+        setJMenuBar(menuBar);
+
+    }
+
+    private void init() {
         JPanel panel = new JPanel();
         panel.setLayout(new GridBagLayout());
         GridBagConstraints gridBagConstraints = new GridBagConstraints();
@@ -22,30 +37,6 @@ public class GameWindow extends JFrame {
         setMinimumSize(new Dimension(600, 500));
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setTitle("Kana");
-        setMenuBars();
     }
 
-    private void setMenuBars() {
-        JMenuBar menuBar = new JMenuBar();
-        JMenu fileMenu = new JMenu("Файл");
-        JMenuItem openItem = new JMenuItem("Открыть");
-        JMenuItem saveItem = new JMenuItem("Coxранить");
-        fileMenu.add(openItem);
-        fileMenu.add(saveItem);
-        menuBar.add(fileMenu);
-        setJMenuBar(menuBar);
-
-
-    }
-
-    private void init() {
-
-    }
-
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            GameWindow gameWindow = new GameWindow();
-            gameWindow.setVisible(true);
-        });
-    }
 }
